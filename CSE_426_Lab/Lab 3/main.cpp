e3ecf945ef95ead4d8e9aec7c9f5596481fd92f9
Rashik_Rahman_Pritom <57052944+RashikRahman@users.noreply.github.com>
2021-08-24 00:03:31 +0600

    week4

--- CSE_426_Lab/Lab 3/main.cpp
+++ CSE_426_Lab/Lab 3/main.cpp
@@ -4,35 +4,9 @@
 #define pi 3.14159265
 
 float r = 0.5;
-
-void display(){
-
-    //National Flag
-    /*
-    glBegin(GL_POLYGON); //Using polygon fills the circle. //GL_POINTS
-    glColor3f(0,1,0);
-    glVertex2f(1,0.6);
-    glVertex2f(1,-0.6);
-    glVertex2f(-1,-0.6);
-    glVertex2f(-1,0.6);
-    glEnd();
-
-
-    glBegin(GL_POLYGON); //Using polygon fills the circle. //GL_POINTS
-    for (float theta=0; theta<360; theta+=0.001){
-        float x = r * cos((theta*2*pi)/360);
-        float y = r * sin((theta*2*pi)/360);
-        glColor3f(1,0,0);
-        glVertex2f(x,y);
-    }
-    glEnd();
-    */
-
-
-
-
+void display2(){
     //Triangle
-    /*
+
     //Upper right
     glBegin(GL_POLYGON);
     glColor3f(0,1,0);
@@ -52,7 +26,7 @@ void display(){
 
     //Upper left
     glBegin(GL_POLYGON);
-    glColor3f(1,0,0);
+    glColor3f(1,0,0); //RGB
     glVertex2f(-0.15,0.25);
     glVertex2f(-0.5,0.25);
     glVertex2f(-0.25,0);
@@ -83,8 +57,31 @@ void display(){
     glVertex2f(0,-0.15);
     glVertex2f(-0.25,0);
     glEnd();
+    glFlush();
 
-    */
+}
+void display(){
+
+    //National Flag
+
+    glBegin(GL_POLYGON); //Using polygon fills the circle. //GL_POINTS
+    glColor3f(0,1,0);
+    glVertex2f(1,0.6);//W:H=5:3=1:0.6
+    glVertex2f(1,-0.6);
+    glVertex2f(-1,-0.6);
+    glVertex2f(-1,0.6);
+    glEnd();
+
+
+    glBegin(GL_POLYGON); //Using polygon fills the circle. //GL_POINTS
+    for (float theta=0; theta<360; theta+=0.001){
+        float x = r * cos((theta*2*pi)/360);
+        float y = r * sin((theta*2*pi)/360);
+        glColor3f(1,0,0);
+        glVertex2f(x,y);
+    }
+    glEnd();
+
 
     glFlush();
 
@@ -95,10 +92,11 @@ int main(int argc, char **argv){
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE);
-    glutInitWindowSize(1280,720);
+    glutInitWindowSize(720,720);
     glutInitWindowPosition(500,500);
     glutCreateWindow("rashik");
     glutDisplayFunc(display);
+    //glutDisplayFunc(display2);
     glutMainLoop();
     return 0;
 
---
+++ CSE_426_Lab/lab 4/main.cpp
@@ -0,0 +1,80 @@
+#include<windows.h>
+#include<GL/glut.h>
+#include<math.h>
+#define pi 3.14159265
+
+float xa=0.2,ya=0.2, xb=-0.2,yb=0.2, xc=-0.2,yc=-0.2, xd=0.2,yd=-0.2;
+
+
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT);//This clears out the previous buffer so that we can see a single object moving rather than object overlays.
+
+    glBegin(GL_QUADS);
+    glColor3f(1,0,0);
+    glVertex2f(xa,ya);
+    glColor3f(1,1,0);
+    glVertex2f(xb,yb);
+    glColor3f(0,1,0);
+    glVertex2f(xc,yc);
+    glColor3f(0,0,1);
+    glVertex2f(xd,yd);
+    glEnd();
+
+    glFlush();
+ }
+
+ void keyboard(unsigned char key, int x, int y){
+
+     switch(key){
+        case 'w':
+            glTranslatef(0,0.1,0);
+            glutPostRedisplay();
+            break;
+        case 's':
+            glTranslatef(0,-0.1,0);
+            glutPostRedisplay();
+            break;
+        case 'd':
+            glTranslatef(0.1,0,0);
+            glutPostRedisplay();
+            break;
+
+        case 'a':
+            glTranslatef(-0.1,0,0);
+            glutPostRedisplay();
+            break;
+        case 'q':
+            glRotatef(45,0,0,1);// anticlock wise thus 45
+            glutPostRedisplay();
+            break;
+        case 'e':
+            glRotatef(-45,0,0,1);// clock wise thus -45
+            glutPostRedisplay();
+            break;
+        case 'c':
+            glScalef(2,2,1);// upscale
+            glutPostRedisplay();
+            break;
+        case 'z':
+            glScalef(.5,.5,1);// downscale
+            glutPostRedisplay();
+            break;
+
+     }
+ }
+
+
+int main(int argc, char **argv){
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(720,720);
+    glutInitWindowPosition(200,200);
+    glutCreateWindow("rashik");
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+
+    glutMainLoop();
+    return 0;
+
+ }
